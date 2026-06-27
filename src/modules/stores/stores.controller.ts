434ead1a398f1e0b5c06c71a81d03e28e14176b2
rafioul-hasan-58 <rafioulhasan2@gmail.com>
2026-06-27 09:22:51 +0600

    feat: create store added

--- src/modules/stores/stores.controller.ts
+++ src/modules/stores/stores.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { StoresService } from './stores.service';
+import { CreateStoreDto } from './dto/create-store.dto';
+import { ResponseMessage } from 'src/common/decorators/response-message.decorator';
+import { AuthGuard } from 'src/common/guards/auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UserRole } from '@prisma/client';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 
 @Controller('stores')
-export class StoresController {}
+export class StoresController {
+    constructor(private readonly storeService: StoresService) { }
+
+    // POST: /stores/create-store
+    @Post('/create-store')
+    @UseGuards(AuthGuard, RolesGuard)
+    @Roles(UserRole.CUSTOMER, UserRole.SELLER)
+    @ResponseMessage('Store created successfully')
+    createStore(
+        @Body() dto: CreateStoreDto,
+        @CurrentUser('sub') userId: string,
+    ) {
+        return this.storeService.createStore(userId, dto);
+    }
+}
